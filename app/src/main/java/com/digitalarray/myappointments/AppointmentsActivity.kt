a1d9f90aa72288edfcd7c29e9781ec903945e276
Alex <alexandro858@hotmail.com>
2020-06-07 18:28:56 -0500

    Base project

---
+++ app/src/main/java/com/digitalarray/myappointments/AppointmentsActivity.kt
@@ -0,0 +1,45 @@
+package com.digitalarray.myappointments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.digitalarray.myappointments.model.Appointment
+import kotlinx.android.synthetic.main.activity_appointments.*
+
+class AppointmentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointments)
+
+        val appointments = ArrayList<Appointment>()
+        appointments.add(
+            Appointment(
+                1,
+                "Médico Test",
+                "17/12/2019",
+                "4:00 PM"
+            )
+        )
+        appointments.add(
+            Appointment(
+                2,
+                "Médico Test 2",
+                "17/12/2020",
+                "4:30 PM"
+            )
+        )
+        appointments.add(
+            Appointment(
+                3,
+                "Médico Test 3",
+                "15/12/2019",
+                "5:00 PM"
+            )
+        )
+
+        rvAppointments.layoutManager = LinearLayoutManager(this)
+        rvAppointments.adapter = AppointmentAdapter(appointments)
+    }
+}
---
+++ app/src/main/java/com/digitalarray/myappointments/model/Appointment.kt
@@ -0,0 +1,8 @@
+package com.digitalarray.myappointments.model
+
+data class Appointment (
+    val id: Int,
+    val doctorName: String,
+    val scheduledDate: String,
+    val scheduledTime: String
+)
